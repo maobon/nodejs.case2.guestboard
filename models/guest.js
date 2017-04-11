97ba58f6ff4041329b0e515e89b63b12a75ae09b
maobon <maobonhaha@gmail.com>
2017-04-11 20:43:26 +0800

    记了一些笔记

--- models/guest.js
+++ models/guest.js
@@ -6,19 +6,24 @@
 
 var mongoose = require('mongoose');
 
-// 1. 连接数据库
+
+/**
+ * 1. 连接数据库
+ */
 mongoose.connect('mongodb://localhost/itcast');
 
-// 2. 设计你的模型架构
-// 所谓的架构 为你的集合指定有哪些字段
-// 字段是什么类型 字段的约束是什么?
 
-// Mongoose 设计了对用户传入数据的约束
-// 检查集合中必须要有什么字段之类的
+/**
+ * 2. 设计你的模型架构
+ *
+ * 所谓的架构 为你的集合指定有哪些字段,字段是什么类型, 字段的约束是什么
+ * Mongoose 设计了对用户传入数据的约束
+ * 检查集合中必须要有什么字段之类的
+ */
 var guestSchema = mongoose.Schema({
     ip: {
         type: String,
-        required: true
+        required: true // 必须写入该字段 不能为空
     },
     message: {
         type: String,
@@ -31,15 +36,20 @@ var guestSchema = mongoose.Schema({
     }
 });
 
-// 3. 将架构发布为模型
-// 参数1: 指定你的集合名 (一般集合名使用小写的复数形式)
-// mongoose.model()传入一个大写字母开头的单词 mongoose创建集合的时候 转为小写
-// 参数2: 给定一个架构 schema
+
+/**
+ * 3. 将架构发布为模型
+ * 参数1: 指定你的集合名 (一般集合名使用小写的复数形式)
+ * mongoose.model()传入一个大写字母开头的单词 mongoose创建集合的时候 转为小写
+ * 参数2: 给定一个架构 schema
+ */
 var Guest = mongoose.model('Guest', guestSchema); // 接收返回值, 首字母大写 看起来类似构造函数
+
 
 // 4. 直接通过Guest或者guest的实例操作数据库
 // new Guest({
-//     ip: '123'
+//     ip: '123',
+//     message: 'hahah'
 // }).save(function (err, result) {
 //     if (err) throw err;
 //     console.log(result);
